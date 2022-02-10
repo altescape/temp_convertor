caee4379f7cb55777a842181f6c01f8138695001
altescape <hello@michaelwatts.me>
2022-02-10 19:50:49 +0000

    Update and tidy

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,56 @@
-use dialoguer::{theme::ColorfulTheme, Select, Input};
+use dialoguer::{theme::ColorfulTheme, Select, Input, Confirm};
 
 fn main() {
     clearscreen::clear().unwrap();
 
-    let temp: String = Input::with_theme(&ColorfulTheme::default())
-        .with_prompt("Input a temperature to convert:")
-        .interact_text()
-        .unwrap();
+    loop {
+        // Prompt temperature input
+        let temp: String = Input::with_theme(&ColorfulTheme::default())
+            .with_prompt("Input a temperature to convert:")
+            .interact_text()
+            .unwrap();
 
-    clearscreen::clear().unwrap();
+        clearscreen::clear().unwrap();
 
-    let temp: f32 = temp.trim().parse().expect("A number is required");
+        // Prompt select temperature type
+        let temp: f32 = temp.trim().parse().expect("A number is required");
+        let temp_types = &["Fahrenheit", "Celsius"];
+        let temp_type = Select::with_theme(&ColorfulTheme::default())
+            .with_prompt(format!("Convert temperature {}° to...", temp))
+            .default(0)
+            .items(&temp_types[..])
+            .interact()
+            .unwrap();
 
-    let temp_types = &["Fahrenheit", "Celsius"];
+        clearscreen::clear().unwrap();
 
-    let temp_type = Select::with_theme(&ColorfulTheme::default())
-        .with_prompt(format!("Convert temperature {}° to...", temp))
-        .default(0)
-        .items(&temp_types[..])
-        .interact()
-        .unwrap();
+        // Output conversion
+        match temp_types[temp_type] {
+            "Fahrenheit" => println!("Temperature is {:.1}°F", celsius_to_fahrenheit(temp)),
+            "Celsius" => println!("Temperature is {:.1}°C", fahrenheit_to_celsius(temp)),
+            _ => println!("Temperature type not recognised"),
+        }
 
-    clearscreen::clear().unwrap();
-
-    match temp_types[temp_type] {
-        "Fahrenheit" => println!("Temperature is {:.1}°F", celsius_to_fahrenheit(temp)),
-        "Celsius" => println!("Temperature is {:.1}°C", fahrenheit_to_celsius(temp)),
-        _ => println!("Temperature type not recognised"),
+        // Prompt continue or quit
+        match Confirm::with_theme(&ColorfulTheme::default())
+        .with_prompt("Convert another?")
+        .default(true)
+        .wait_for_newline(true)
+        .interact_opt()
+        .unwrap()
+        {
+            Some(true) => {
+                clearscreen::clear().unwrap();
+                continue;
+            },
+            Some(false) => {
+                clearscreen::clear().unwrap();
+                break;
+            },
+            None => break,
+        }
     }
+
 }
 
 fn celsius_to_fahrenheit(temp: f32) -> f32 {
