90d74898f55f4aa82a3dc5eb953aa3548f9e380b
altescape <hello@michaelwatts.me>
2022-02-10 18:21:27 +0000

    Improve UX

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,31 @@
 use dialoguer::{theme::ColorfulTheme, Select, Input};
 
 fn main() {
-    print!("{esc}c", esc = 27 as char);
+    clearscreen::clear().unwrap();
 
     let temp: String = Input::with_theme(&ColorfulTheme::default())
         .with_prompt("Input a temperature to convert:")
         .interact_text()
         .unwrap();
 
-    print!("{esc}c", esc = 27 as char);
+    clearscreen::clear().unwrap();
 
     let temp: f32 = temp.trim().parse().expect("A number is required");
 
-    let temp_types = &["Fahrenheit to Celsius", "Celsius to Fahrenheit"];
+    let temp_types = &["Fahrenheit", "Celsius"];
 
     let temp_type = Select::with_theme(&ColorfulTheme::default())
-        .with_prompt("Convert:")
+        .with_prompt(format!("Convert temperature {}° to...", temp))
         .default(0)
         .items(&temp_types[..])
         .interact()
         .unwrap();
 
+    clearscreen::clear().unwrap();
+
     match temp_types[temp_type] {
-        "Celsius to Fahrenheit" => println!("Temperature is {:.1}°F", celsius_to_fahrenheit(temp)),
-        "Fahrenheit to Celsius" => println!("Temperature is {:.1}°C", fahrenheit_to_celsius(temp)),
+        "Fahrenheit" => println!("Temperature is {:.1}°F", celsius_to_fahrenheit(temp)),
+        "Celsius" => println!("Temperature is {:.1}°C", fahrenheit_to_celsius(temp)),
         _ => println!("Temperature type not recognised"),
     }
 }
