c4e675eacec56e43fe7901f30e71728efdbee3dd
altescape <hello@michaelwatts.me>
2022-03-22 13:33:00 +0000

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,11 @@ fn main() {
     clearscreen::clear().unwrap();
 
     loop {
-        // Prompt temperature input
-        let temp: String = Input::with_theme(&ColorfulTheme::default())
-            .with_prompt("Input a temperature to convert:")
-            .interact_text()
-            .unwrap();
+        let temp: f32 = prompt_temp_input();
 
         clearscreen::clear().unwrap();
 
         // Prompt select temperature type
-        let temp: f32 = temp.trim().parse().expect("A number is required");
         let temp_types = &["Fahrenheit", "Celsius"];
         let temp_type = Select::with_theme(&ColorfulTheme::default())
             .with_prompt(format!("Convert temperature {}° to...", temp))
@@ -21,42 +16,76 @@ fn main() {
             .items(&temp_types[..])
             .interact()
             .unwrap();
+        prompt_select_type(&temp, &temp_types);
 
         clearscreen::clear().unwrap();
 
         // Output conversion
-        match temp_types[temp_type] {
-            "Fahrenheit" => println!("Temperature is {:.1}°F", celsius_to_fahrenheit(temp)),
-            "Celsius" => println!("Temperature is {:.1}°C", fahrenheit_to_celsius(temp)),
-            _ => println!("Temperature type not recognised"),
-        }
+        let result = match temp_types[temp_type] {
+            "Fahrenheit" => Temp::F(temp),
+            "Celsius" => Temp::C(temp),
+            _ => Temp::None(),
+        };
+
+        print_temp(&result);
 
         // Prompt continue or quit
         match Confirm::with_theme(&ColorfulTheme::default())
-        .with_prompt("Convert another?")
-        .default(true)
-        .wait_for_newline(true)
-        .interact_opt()
-        .unwrap()
-        {
-            Some(true) => {
-                clearscreen::clear().unwrap();
-                continue;
-            },
-            Some(false) => {
-                clearscreen::clear().unwrap();
-                break;
-            },
-            None => break,
-        }
+            .with_prompt("Convert another?")
+            .default(true)
+            .wait_for_newline(true)
+            .interact_opt()
+            .unwrap()
+            {
+                Some(true) => {
+                    clearscreen::clear().unwrap();
+                    continue;
+                },
+                Some(false) => {
+                    clearscreen::clear().unwrap();
+                    break;
+                },
+                None => break,
+            }
     }
-
 }
 
-fn celsius_to_fahrenheit(temp: f32) -> f32 {
-    (temp * 1.8) + 32.0
+enum Temp {
+    F(f32),
+    C(f32),
+    None(),
 }
 
-fn fahrenheit_to_celsius(temp: f32) -> f32 {
-    ((temp - 32.0) * 5.0) / 9.0
+fn prompt_temp_input() -> f32 {
+    let temp: String = Input::with_theme(&ColorfulTheme::default())
+        .with_prompt("Input a temperature to convert:")
+        .interact_text()
+        .unwrap();
+
+    temp.trim().parse().expect("A number is required")
+}
+
+fn prompt_select_type(temp: &f32, temp_types: &[&str; 2]) {
+    Select::with_theme(&ColorfulTheme::default())
+        .with_prompt(format!("Convert temperature {}° to...", temp))
+        .default(0)
+        .items(&temp_types[..])
+        .interact()
+        .unwrap();
+}
+
+fn convert_temp(temp: &Temp) -> f32 {
+    match temp {
+        &Temp::F(degrees) => (degrees * 1.8) + 32.0,
+        &Temp::C(degrees) => ((degrees - 32.0) * 5.0) / 9.0,
+        _ => 0.0
+    }
+}
+
+fn print_temp(temp: &Temp) {
+    match temp {
+        &Temp::F(degrees) => println!("{:.0}°F = {:.0}°C", degrees, convert_temp(temp)),
+        &Temp::C(degrees) => println!("{:.0}°C = {:.0}°F", degrees, convert_temp(temp)),
+        _ => println!("Conversion not performed")
+    }
 }
